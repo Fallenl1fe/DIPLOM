50ceaa335b348ebfa30c52b96428e4003149f55e
Alexandr Z <gopokemon1739@gmail.com>
2021-05-21 16:13:03 +0300

    говняк

--- app.py
+++ app.py
@@ -7,19 +7,25 @@ from datetime import datetime
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'secret-key-goes-here'
+app.config['SECRET_KEY'] = 'iuhiuh'
+from models import db
+import models
 
-from models import Posts, Users, db
+login_manager = LoginManager()
+login_manager.init_app(app)
 
+@login_manager.user_loader
+def load_user(user_id):
+    return models.Users.get(user_id)
 
 @app.route("/")
 def index():
     return render_template("index.html", title="Главная")
 
+@app.route("/")
+def login():
+    return render_template("login.html", title="Логинка")
 
-@app.route('/profile')
-def profile():
-    return render_template("profile.html", title="Профиль")
 
 @app.route('/logout')
 def logout():
@@ -27,21 +33,13 @@ def logout():
     flash("Вы вышли из аккаунта", "success")
     return redirect(url_for('login'))
 
-# @app.route("/post/<alias>")
-# #@login_required
-# def showPost(alias):
-#     title, post = db.getPost(alias)
-#     if not title:
-#         abort(404)
-#
-#     return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
 
 
 @app.route("/add_post", methods=("POST", "GET"))
 def add_post():
     if request.method == "POST":
         try:
-            p = Posts(name=request.form['name'], text=request.form['postes'])
+            p = models.Posts(name=request.form['name'], text=request.form['postes'])
             db.session.add(p)
             db.session.flush()
 
@@ -49,8 +47,8 @@ def add_post():
         except:
             db.session.rollback()
             print("Ошибка добавления в БД")
-
-        return redirect(url_for('index'))
+        flash("Пост создан", category='success')
+        return redirect(url_for('add_post'))
 
     return render_template("add_post.html", title="Регистрация")
 
@@ -61,7 +59,8 @@ def register():
         # здесь должна быть проверка корректности введенных данных
         try:
             hash = generate_password_hash(request.form['psw'])
-            u = Users(email=request.form['email'], psw=hash, name=request.form['name'],surname=request.form['surname'],login=request.form['login'])
+            u = models.Users(email=request.form['email'], psw=hash, name=request.form['name'],
+                      surname=request.form['surname'], login=request.form['login'])
             db.session.add(u)
             db.session.flush()
 
@@ -69,11 +68,12 @@ def register():
         except:
             db.session.rollback()
             print("Ошибка добавления в БД")
-
-        return redirect(url_for('register.html'))
+        flash("Готово!", category='success')
+        return redirect(url_for('register'))
 
     return render_template("register.html", title="Регистрация")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
--- models.py
+++ models.py
@@ -1,36 +1,117 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKeyConstraint
+
 from app import app
 
 db = SQLAlchemy(app=app)
 
 
-
 class Users(db.Model):
-
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     login = db.Column(db.String(50), unique=True)
     psw = db.Column(db.String(500), nullable=True)
-    devision_id = db.Column(db.Integer)
+    devision_id = db.Column(db.Integer, db.ForeignKey('devisions.id'))
     email = db.Column(db.String(50), unique=True)
     name = db.Column(db.String(50))
     surname = db.Column(db.String(50))
     role = db.Column(db.Boolean, default=0)
-    teststatus = db.Column(db.Boolean, default=0)
-    tested = db.Column(db.Boolean, default=0)
     rating = db.Column(db.Float, default=0)
 
-    pr = db.relationship('Posts', backref='users', uselist=False)
+    #post_rl = db.relationship('Posts', backref='users_posts')
+    #chat_rl = db.relationship('Chats', backref='users_chats')
 
     def __repr__(self):
         return f"<users {self.id}>"
 
-class Posts(db.Model):
 
+class Posts(db.Model):
+    __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=True)
     text = db.Column(db.Text, nullable=True)
-
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     def __repr__(self):
-        return f"<profiles {self.id}>"
+        return f"<posts {self.id}>"
+
+
+class Devisions(db.Model):
+    __tablename__ = 'devisions'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=True)
+
+    #devis_rl = db.relationship('devisions', backref='devis_users')
+
+    def __repr__(self):
+        return f"<devisions {self.id}>"
+
+
+class Chats(db.Model):
+    __tablename__ = 'chats'
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    telegram_user_id = db.Column(db.Integer, primary_key=True)
+
+
+class Temas(db.Model):
+    __tablename__ = 'temas'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=True)
+
+
+class Tests(db.Model):
+    __tablename__ = 'tests'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    date_create = db.Column(db.DateTime)
+    max_test_time = db.Column(db.Time)
+    max_q = db.Column(db.Integer)
+    tema_id = db.Column(db.Integer, db.ForeignKey('temas.id'))
+
+
+class Questions(db.Model):
+    __tablename__ = 'questions'
+    id = db.Column(db.Integer, primary_key=True)
+    q_text = db.Column(db.Text)
+    test_id = db.Column(db.Integer, db.ForeignKey('tests.id'))
+
+
+class Answers(db.Model):
+    __tablename__ = 'answers'
+    id = db.Column(db.Integer, primary_key=True)
+    question_id = db.Column(db.Integer, db.ForeignKey('questions.id'))
+    a_text = db.Column(db.String(100))
+    correct_a = db.Column(db.Boolean)
+
+
+class Protocols(db.Model):
+    __tablename__ = 'protocols'
+
+    answer_id = db.Column(db.Integer, db.ForeignKey('answers.id'), primary_key=True)
+    testing_id = db.Column(db.Integer, db.ForeignKey('answers.id'), primary_key=True)
+
+
+# Добавить поля связей
+# по типу devis_rl = db.relationship('devisions', backref='devis_users')
+
+
+class Testing(db.Model):
+    __tablename__ = 'testing'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    test_id = db.Column(db.Integer, db.ForeignKey('tests.id'))
+    start_date = db.Column(db.DateTime, nullable=True)
+    end_date = db.Column(db.DateTime, nullable=True)
+    rating = db.Column(db.Float)
+    result = db.Column(db.Boolean)
+    a_number = db.Column(db.Integer)
+
+
+class User_test(db.Model):
+    __tablename__ = 'user_test'
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    test_id = db.Column(db.Integer, db.ForeignKey('tests.id'), primary_key=True)
+
+
+db.create_all()
